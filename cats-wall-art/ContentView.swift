05d79dfd6fe08ba9949bd769c23ac2c002177511
Zach Plata <plata.zach@gmail.com>
2023-12-11 09:47:20 -0600

    Initial Commit

---
+++ cats-wall-art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cats-wall-art
+//
+//  Created by Zach Plata on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cats-wall-art/cats_wall_artApp.swift
@@ -0,0 +1,17 @@
+//
+//  cats_wall_artApp.swift
+//  cats-wall-art
+//
+//  Created by Zach Plata on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct cats_wall_artApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
